fe8b34fc28fafd4a450624836116879351576e77
William Fank Thomé <willegp88@gmail.com>
2020-10-30 22:51:46 -0300

    feat: create object helper

---
+++ src/helpers/object.helper.ts
@@ -0,0 +1,22 @@
+import { PropertyName } from '@/types'
+import { ERROR_MESSAGES } from '@/constants'
+
+export const defineObjectProperty = <T extends Record<string, any>, V> (
+  toUpdate: T,
+  propertyName: PropertyName,
+  value: V
+): T => {
+  if (!toUpdate)
+    throw new Error(ERROR_MESSAGES.OBJECT_IS_FALSY)
+
+  if (!(propertyName in toUpdate))
+    throw new Error(ERROR_MESSAGES.UNKNOWN_PROPERTY(propertyName))
+
+  const descriptor = Object.getOwnPropertyDescriptor(toUpdate, propertyName)
+  Object.defineProperty(toUpdate, propertyName, {
+    ...descriptor,
+    value
+  })
+
+  return toUpdate
+}
