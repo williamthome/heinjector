708bacad186305d0873cdbdb9b652ee3fdaa1430
William Fank Thomé <willegp88@gmail.com>
2020-10-30 22:20:15 -0300

    feat: create constructor helper

---
+++ src/helpers/constructor.helper.ts
@@ -0,0 +1,9 @@
+import { Constructor } from '@/types'
+
+export const getArgumentNames = <T> (constructor: Constructor<T>): string[] => {
+  const RegExInsideParentheses = /[(][^)]*[)]/
+  const RegExParenthesesAndSpaces = /[()\s]/g
+  const regExValue = RegExInsideParentheses.exec(constructor.toString())
+  if (!regExValue) return []
+  else return regExValue[0].replace(RegExParenthesesAndSpaces, '').split(',').map(str => str.trim())
+}
