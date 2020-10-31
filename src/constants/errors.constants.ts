449852dbdf890c5c5f26693c5fe80cef92547b5c
William Fank Thomé <willegp88@gmail.com>
2020-10-31 01:47:49 -0300

    refactor: add cache is falsy error message

--- src/constants/errors.constants.ts
+++ src/constants/errors.constants.ts
@@ -5,11 +5,18 @@ export const ERROR_MESSAGES = {
   UNKNOWN_IDENTIFIER: <T> (
     identifier: Identifier<T>
   ): string => `Unknown identifier ${identifierToString(identifier)}`,
+
   UNKNOWN_IDENTIFIER_NAME: (
     name: PropertyName
   ): string => `Unknown name ${name.toString()} for identifier`,
+
   UNKNOWN_PROPERTY: (
     property: PropertyName
   ): string => `Unknown property ${property.toString()} for object`,
-  OBJECT_IS_FALSY: 'Object is null or undefined'
+
+  OBJECT_IS_FALSY: 'Object is null or undefined',
+
+  CACHE_IS_FALSY: <T> (
+    identifier: Identifier<T>
+  ): string => `Cache is null or undefined for identifier ${identifierToString(identifier)}`,
 }
