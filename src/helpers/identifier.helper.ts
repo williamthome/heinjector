b414549db21d9809b048e53a213384f5ec452c25
William Fank Thomé <willegp88@gmail.com>
2020-10-30 22:23:50 -0300

    feat: create identifier helper

---
+++ src/helpers/identifier.helper.ts
@@ -0,0 +1,18 @@
+import { isProxy } from './proxy.helper'
+import { Constructor, Identifier } from '@/types'
+
+export const identifierToString = (identifier: Identifier<any>): string => {
+  switch (typeof identifier) {
+    case 'string':
+      return identifier
+    case 'symbol':
+      return identifier.toString()
+    default:
+      return identifier.name
+  }
+}
+
+export const identifierAsMapKey = <T> (identifier: Identifier<T>): Identifier<T> =>
+  isProxy(identifier)
+    ? (identifier as Constructor<T>).prototype.constructor
+    : identifier
