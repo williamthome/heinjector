86c5756db05f1da15c9644e2e9db30afe4198794
William Fank Thomé <willegp88@gmail.com>
2020-11-02 20:47:48 -0300

    fix: types for constructor and identifier are not the same

--- src/decorators/inject.decorator.ts
+++ src/decorators/inject.decorator.ts
@@ -4,15 +4,15 @@ import { makeRegisterOptions } from '@/helpers/register.helper'
 import { InjectOptions } from '@/interfaces'
 import { Constructor } from '@/types'
 
-export const Inject = <P = any> (options?: InjectOptions<P>) => {
-  return <T extends Constructor<P>> (
+export const Inject = <TConstructor = any, TIdentifier = any> (options?: InjectOptions<TIdentifier>) => {
+  return <T extends Constructor<TConstructor>> (
     constructor: T,
     _propertyKey: string | symbol,
     parameterIndex: number
   ): void => {
-    const propertyName = getArgumentNames<P>(constructor)[parameterIndex]
+    const propertyName = getArgumentNames<TConstructor>(constructor)[parameterIndex]
 
-    const { identifier, isArray } = makeRegisterOptions<P>(propertyName, options)
+    const { identifier, isArray } = makeRegisterOptions<TIdentifier>(propertyName, options)
 
     heinjector.register({
       identifier,
