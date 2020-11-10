77af3f494c258a7b489c52b3c5b710269eaff847
William Fank Thomé <willegp88@gmail.com>
2020-11-10 00:36:30 -0300

    refactor: identifier for registered options must be array

--- src/types/registered.type.ts
+++ src/types/registered.type.ts
@@ -1,5 +1,7 @@
 import { RegisterOptions } from '.'
+import { Identifier } from './identifier.type'
 
 export type Registered<T> =
-  Omit<RegisterOptions<T>, 'dependencies'> &
-  Required<Pick<RegisterOptions<T>, 'dependencies'>>
+  Omit<RegisterOptions<T>, 'dependencies' | 'identifier'> &
+  Required<Pick<RegisterOptions<T>, 'dependencies'>> &
+  { identifier: Array<Identifier<T>> }
